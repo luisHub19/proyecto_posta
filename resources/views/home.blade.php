ee2c01e5c909227be95293526dc82dfd9f846f30
baesieluv7 <manuel.delgado.isw@unipolidgo.edu.mx>
2024-04-15 22:07:40 -0600

    Final

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,6 @@
 @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 @vite(['resources/sass/_seccionado-home.scss'])
 @extends('layouts.index')
-$notas = App\Models\Noticia::orderBy('created_at', 'desc')->take(3)->get();
 @section('content')
  
 @component('components.SecDelDia', ['notas' => $notas])
--- resources/views/nota.blade.php
+++ resources/views/nota.blade.php
@@ -2,5 +2,4 @@
 @extends('layouts.index')
 @section('content')
     @component('components.noticiaC', ['notas' => $notas])@endcomponent
-    @component('components.notamue', ['notas' => $notas])@endcomponent
 @endsection
